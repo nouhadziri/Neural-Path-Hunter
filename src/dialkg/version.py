26fc0d07213f3d7bd73aa8b953bf786747ebf7ac
Nouha Dziri <dziri@ualberta.ca>
2021-11-21 17:50:34 -0700

    Delete src directory

--- src/dialkg/version.py
+++
@@ -1,9 +0,0 @@
-_MAJOR = "0"
-_MINOR = "2"
-# On main and in a nightly release the patch should be one ahead of the last
-# released build.
-_PATCH = "3"
-
-
-VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
-VERSION = "{0}.{1}.{2}".format(_MAJOR, _MINOR, _PATCH)
